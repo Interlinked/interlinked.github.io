5846402ebacc438e56180599899d7141ba5e22ee
Interlinked <130935054+Interlinked@users.noreply.github.com>
2024-09-25 02:09:10 +0000

    horrible

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 function oatify(){
-    alert("hi");
-    var userInput = document.getElementById("oatInput").value;
-    var userInput = userInput.replaceAll("o","oat")
-    var userInput = userInput.replaceAll("O","Oat")
     
-    alert(userInput);
+    var userInput = document.getElementById("oatInput").value;
+    var userInput = userInput.replaceAll("o","oat");
+    var userInput = userInput.replaceAll("O","Oat");
+    document.getElementById("oatOutput").value = userInput;
+    
 };
+function changeButton() {
+    document.getElementById("copyButton").value = "copy to clipboard";
+};
+function  copyOut(){
+    var copyText = document.getElementById("oatOutput");
+
+    
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+  
+     
+    navigator.clipboard.writeText(copyText.value);
+    document.getElementById("copyButton").value = "copied!";
+    setTimeout(changeButton,1000);
+};
+
+
